66f926ad25c1c20a0feee4142c5a690e722cefe3
gusta356 <speedgu7@gmail.com>
2026-05-09 15:00:35 -0300

    entradas em python

---
+++ 03_python_entrada/entrada_01.py
@@ -0,0 +1,10 @@
+# autor: gustavo martins
+#projeto:entradas em phyton
+
+# declarar variáveis
+valor1 = 6
+valor2 = 2
+soma = valor1 + valor2
+
+#exibir resultados
+print("a soma é: ",soma)
---
+++ 03_python_entrada/entrada_02.py
@@ -0,0 +1,10 @@
+#autor: gustavo martins
+#projeto: entrada com input
+
+#declaração de variáveis
+valor1 = int(input("digite o primeiro valor: "))
+valor2 = int(input("digite o segundo valor: "))
+soma = valor1 + valor2
+
+#exibindo os resultados 
+print("o resultado da soma é ", soma)
